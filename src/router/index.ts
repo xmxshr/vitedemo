c022dfeea816794907f2c5f7b92ce86cb5c62825
xmx <xumanxi626@hotmail.com>
2021-07-21 14:38:45 +0800

    init

--- src/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    { path: '/', component: () => import('../views/home.vue') },
-  ],
-})
-
-export default router;
